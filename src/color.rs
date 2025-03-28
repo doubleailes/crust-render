7f3b03a883bf678b6e28dfd72670ba0a59018e24
Philippe Llerena <philippe.llerena@forticheprod.com>
2025-03-28 10:58:41 +0100

    Refactor color handling and implement sphere intersection for ray tracing

--- src/color.rs
+++ src/color.rs
@@ -1,14 +1,16 @@
-use std::io::Write;
-
 use crate::vec3::Vec3;
 
 // Type alias
 pub type Color = Vec3;
 
-pub fn write_color(out: &mut impl Write, pixel_color: Color) {
-    // Write the translated [0, 255] value of each color component
-    let r = (255.999 * pixel_color.x()) as i32;
-    let g = (255.999 * pixel_color.y()) as i32;
-    let b = (255.999 * pixel_color.z()) as i32;
-    writeln!(out, "{} {} {}", r, g, b).expect("writing color");
+impl Color {
+    pub fn r(&self) -> f32 {
+        self.x()
+    }
+    pub fn g(&self) -> f32 {
+        self.y()
+    }
+    pub fn b(&self) -> f32 {
+        self.z()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,50 @@ mod ray;
 mod vec3;
 
 use color::Color;
+use exr::prelude::*;
 use ray::Ray;
-use std::io;
 use vec3::{Point3, Vec3};
 
+const ASPECT_RATIO: f32 = 16.0 / 9.0;
+const IMAGE_WIDTH: usize = 400;
+const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as usize;
+
+fn hit_sphere(center: Point3, radius: f32, r: &Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = vec3::dot(r.direction(), r.direction());
+    let b = 2.0 * vec3::dot(oc, r.direction());
+    let c = vec3::dot(oc, oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant >= 0.0
+}
+
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
     let unit_direction: Vec3 = vec3::unit_vector(r.direction());
     let t: f32 = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
-fn main() {
-    // Image
 
-    const ASPECT_RATIO: f32 = 16.0 / 9.0;
-    const IMAGE_WIDTH: i32 = 400;
-    const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as i32;
+fn get_color(
+    x: usize,
+    y: usize,
+    origin: Vec3,
+    lower_left_corner: Vec3,
+    horizontal: Vec3,
+    vertical: Vec3,
+) -> (f32, f32, f32, f32) {
+    let u = x as f32 / (IMAGE_WIDTH - 1) as f32;
+    let v = y as f32 / (IMAGE_HEIGHT - 1) as f32;
+    let r = Ray::new(
+        origin,
+        lower_left_corner + u * horizontal + v * vertical - origin,
+    );
+    let pixel_color = ray_color(&r);
+    (pixel_color.x(), pixel_color.y(), pixel_color.z(), 1.0)
+}
+fn main() {
 
     // Camera
 
@@ -27,26 +56,15 @@ fn main() {
 
     let origin = Point3::new(0.0, 0.0, 0.0);
     let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    // Viewport is flipped vertically to start from the top-left corner
+    // Due to exr crate's coordinate system
+    let vertical = Vec3::new(0.0, -viewport_height, 0.0);
     let lower_left_corner =
         origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     // Render
-
-    print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
-
-    for j in (0..IMAGE_HEIGHT).rev() {
-        eprint!("\rScanlines remaining: {} ", j);
-        for i in 0..IMAGE_WIDTH {
-            let u = i as f32 / (IMAGE_WIDTH - 1) as f32;
-            let v = j as f32 / (IMAGE_HEIGHT - 1) as f32;
-            let r = Ray::new(
-                origin,
-                lower_left_corner + u * horizontal + v * vertical - origin,
-            );
-            let pixel_color = ray_color(&r);
-            color::write_color(&mut io::stdout(), pixel_color);
-        }
-    }
-    eprint!("\nDone.\n");
+    write_rgba_file("output.exr", IMAGE_WIDTH, IMAGE_HEIGHT, |x, y| {
+        get_color(x, y, origin, lower_left_corner, horizontal, vertical)
+    })
+    .expect("writing image");
 }
