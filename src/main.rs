365f0ab8ec16851ce0920193933c069063edb8a8
Philippe Llerena <philippe.llerena@forticheprod.com>
2025-03-28 23:17:49 +0100

    Refactor Camera initialization and ray generation; update material handling in main

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,11 @@ use color::Color;
 use exr::prelude::*;
 use hittable::{HitRecord, Hittable};
 use hittable_list::HittableList;
+use material::{Dielectric, Lambertian, Metal};
 use ray::Ray;
 use sphere::Sphere;
-use vec3::{Point3, Vec3};
-use material::{Lambertian, Metal, Dielectric};
 use std::sync::Arc;
+use vec3::{Point3, Vec3};
 
 // Constants
 
@@ -33,6 +33,7 @@ fn ray_color(r: &Ray, world: &dyn Hittable, depth: i32) -> Color {
     if depth <= 0 {
         return Color::new(0.0, 0.0, 0.0);
     }
+
     let mut rec = HitRecord::new();
     if world.hit(r, 0.001, common::INFINITY, &mut rec) {
         let mut attenuation = Color::default();
@@ -47,28 +48,18 @@ fn ray_color(r: &Ray, world: &dyn Hittable, depth: i32) -> Color {
         }
         return Color::new(0.0, 0.0, 0.0);
     }
-    let unit_direction: Vec3 = vec3::unit_vector(r.direction());
-    let t: f32 = 0.5 * (unit_direction.y() + 1.0);
+
+    let unit_direction = vec3::unit_vector(r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
-fn get_color(
-    world: &dyn Hittable,
-    x: usize,
-    y: usize,
-    origin: Vec3,
-    lower_left_corner: Vec3,
-    horizontal: Vec3,
-    vertical: Vec3,
-) -> (f32, f32, f32, f32) {
-    let mut pixel_color:Vec3  = Color::new(0.0, 0.0, 0.0);
+fn get_color(cam: &Camera, world: &dyn Hittable, x: usize, y: usize) -> (f32, f32, f32, f32) {
+    let mut pixel_color: Vec3 = Color::new(0.0, 0.0, 0.0);
     for _ in 0..SAMPLES_PER_PIXEL {
         let u = (x as f32 + common::random()) / (IMAGE_WIDTH - 1) as f32;
         let v = (y as f32 + common::random()) / (IMAGE_HEIGHT - 1) as f32;
-        let r = Ray::new(
-            origin,
-            lower_left_corner + u * horizontal + v * vertical - origin,
-        );
+        let r = cam.get_ray(u, v);
         pixel_color += ray_color(&r, world, MAX_DEPTH);
     }
     (
@@ -86,7 +77,8 @@ fn main() {
     let material_center = Arc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
     let material_left = Arc::new(Dielectric::new(1.5));
     let material_right = Arc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
- 
+    let r = f32::cos(common::PI / 4.0);
+
     world.add(Box::new(Sphere::new(
         Point3::new(0.0, -100.5, -1.0),
         100.0,
@@ -110,31 +102,17 @@ fn main() {
 
     // Camera
 
-    let cam = Camera::new();
-
-    let viewport_height = 2.0;
-    let viewport_width = ASPECT_RATIO * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Point3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    // Viewport is flipped vertically to start from the top-left corner
-    // Due to exr crate's coordinate system
-    let vertical = Vec3::new(0.0, -viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+    let cam = Camera::new(
+        Point3::new(-2.0, 2.0, 1.0),
+        Point3::new(0.0, 0.0, -1.0),
+        Vec3::new(0.0, 1.0, 0.0),
+        20.0,
+        ASPECT_RATIO,
+    );
 
     // Render
     write_rgba_file("output.exr", IMAGE_WIDTH, IMAGE_HEIGHT, |x, y| {
-        get_color(
-            &world,
-            x,
-            y,
-            origin,
-            lower_left_corner,
-            horizontal,
-            vertical,
-        )
+        get_color(&cam, &world, x, y)
     })
     .expect("writing image");
     convert::convert();
