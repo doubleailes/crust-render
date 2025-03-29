9633b9911f21faa473f71e6dbedf3e040163f67a
Philippe Llerena <philippe.llerena@forticheprod.com>
2025-03-30 00:20:41 +0100

    Add benchmarking for simple world scene generation

--- benches/integrator.rs
+++ benches/integrator.rs
@@ -1,5 +1,15 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use ray_tracing::vec3::Vec3;
+use ray_tracing::world::simple_scene;
+use ray_tracing::camera::Camera;
+use ray_tracing::tracer::{RenderSettings, Renderer};
+use ray_tracing::vec3::Point3;
+
+const ASPECT_RATIO: f32 = 16.0 / 9.0;
+const IMAGE_WIDTH: usize = 400;
+const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as usize;
+const MIN_SAMPLES: u32 = 8;
+const VARIANCE_THRESHOLD: f32 = 0.0005; // You can tweak this!
 
 fn bench_dot(c: &mut Criterion) {
     let vec1 = Vec3::new(1.0, 2.0, 3.0);
@@ -12,5 +22,31 @@ fn bench_dot(c: &mut Criterion) {
     });
 }
 
-criterion_group!(name = benches;config = Criterion::default(); targets= bench_dot);
+fn bench_simple_world(c: &mut Criterion) {
+    c.bench_function("simple world", |b| {
+        b.iter(|| {
+            let (world, lights) = simple_scene();
+            let lookfrom = Point3::new(13.0, 2.0, 3.0);
+            let lookat = Point3::new(0.0, 0.0, 0.0);
+            let vup = Point3::new(0.0, 1.0, 0.0);
+            let dist_to_focus = 10.0;
+            let aperture = 0.1;
+        
+            let cam = Camera::new(
+                lookfrom,
+                lookat,
+                vup,
+                20.0,
+                ASPECT_RATIO,
+                aperture,
+                dist_to_focus,
+            );
+            let render_settings = RenderSettings::new(10, 20, IMAGE_WIDTH, IMAGE_HEIGHT, MIN_SAMPLES, VARIANCE_THRESHOLD);
+            let renderer = Renderer::new(cam, world, lights, render_settings);
+            let _ = renderer.render();
+        })
+    });
+}
+
+criterion_group!(name = benches;config = Criterion::default(); targets= bench_dot,bench_simple_world);
 criterion_main!(benches);
