1c6fa7f5c69e0575906fce50948b085e5a63a6d3
Philippe Llerena <philippe.llerena@forticheprod.com>
2025-04-05 10:21:58 +0200

    Refactor hit method in AABB to use mutable t_min and t_max for clarity

--- crates/crust-render/src/aabb.rs
+++ crates/crust-render/src/aabb.rs
@@ -29,19 +29,19 @@ impl AABB {
         }
     }
 
-    pub fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> bool {
+    pub fn hit(&self, ray: &Ray, mut t_min: f32, mut t_max: f32) -> bool {
         for a in 0..3 {
             let inv_d = 1.0 / ray.direction()[a];
             let mut t0 = (self.minimum[a] - ray.origin()[a]) * inv_d;
             let mut t1 = (self.maximum[a] - ray.origin()[a]) * inv_d;
-
+    
             if inv_d < 0.0 {
                 std::mem::swap(&mut t0, &mut t1);
             }
-
-            let t_min = t_min.max(t0);
-            let t_max = t_max.min(t1);
-
+    
+            t_min = t_min.max(t0);
+            t_max = t_max.min(t1);
+    
             if t_max <= t_min {
                 return false;
             }
