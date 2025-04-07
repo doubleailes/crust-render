d32998201bdadd467d6dc3ce3f5cbb88b8baa5c4
Philippe Llerena <philippe.llerena@forticheprod.com>
2025-04-07 18:14:57 +0200

    Refactor Sphere struct to use glam::Vec3 for center representation

--- crates/crust-render/src/primitives/sphere.rs
+++ crates/crust-render/src/primitives/sphere.rs
@@ -3,16 +3,16 @@ use crate::hittable::HitRecord;
 use crate::hittable::Hittable;
 use crate::material::Material;
 use crate::ray::Ray;
+use glam::Vec3;
 use std::sync::Arc;
-use utils::{Point3, Vec3};
 
 pub struct Sphere {
-    center: Point3,
+    center: Vec3,
     radius: f32,
     material: Arc<dyn Material>,
 }
 impl Sphere {
-    pub fn new(center: Point3, radius: f32, material: Arc<dyn Material>) -> Self {
+    pub fn new(center: Vec3, radius: f32, material: Arc<dyn Material>) -> Self {
         Self {
             center,
             radius,
@@ -30,7 +30,7 @@ impl Hittable for Sphere {
     fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool {
         let oc = r.origin() - self.center;
         let a = r.direction().length_squared();
-        let half_b = utils::dot(oc, r.direction());
+        let half_b = oc.dot(r.direction());
         let c = oc.length_squared() - self.radius * self.radius;
         let discriminant = half_b * half_b - a * c;
 
