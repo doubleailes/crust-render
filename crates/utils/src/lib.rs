249cd5e6bfae2eef104e9cf316339fda4ceeb04c
Philippe Llerena <philippe.llerena@forticheprod.com>
2025-04-07 18:27:05 +0200

    Fix import statement to include random_in_unit_disk function from common module

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -1,4 +1,4 @@
 mod common;
 pub use common::Lerp;
 pub use common::{balance_heuristic, clamp};
-pub use common::{degrees_to_radians, random, random_range, random2};
+pub use common::{degrees_to_radians, random, random_in_unit_disk, random_range, random2};
