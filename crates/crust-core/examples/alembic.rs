51b377dfcfc7c089ed6c5ddd99101fed2cee3bdb
Philippe Llerena <philippe.llerena@forticheprod.com>
2025-04-22 17:57:02 +0200

    Refactor imports from `crust_render` to `crust_core` in example files for consistency

--- crates/crust-core/examples/alembic.rs
+++ crates/crust-core/examples/alembic.rs
@@ -1,6 +1,6 @@
 use std::path;
 
-use crust_render::{Camera, DocObject, Document, Mat4, MaterialType, ObjectList, Primitive, Vec3A};
+use crust_core::{Camera, DocObject, Document, Mat4, MaterialType, ObjectList, Primitive, Vec3A};
 
 fn main() {
     const ASPECT_RATIO: f32 = 16.0 / 9.0;
@@ -22,7 +22,7 @@ fn main() {
         dist_to_focus,
     );
     let render_settings =
-        crust_render::RenderSettings::new(64, 32, IMAGE_WIDTH, IMAGE_HEIGHT, 32, 0.0, 0);
+        crust_core::RenderSettings::new(64, 32, IMAGE_WIDTH, IMAGE_HEIGHT, 32, 0.0, 0);
     let mut object_list = ObjectList::new(vec![]);
     // Add objects to the object_list here
     let ground: DocObject = DocObject::new(
@@ -31,12 +31,12 @@ fn main() {
             center: Vec3A::new(0.0, -1000.0, 0.0),
             radius: 1000.0,
         },
-        MaterialType::Lambertian(crust_render::Lambertian::new(Vec3A::new(0.5, 0.5, 0.5))),
+        MaterialType::Lambertian(crust_core::Lambertian::new(Vec3A::new(0.5, 0.5, 0.5))),
     );
     object_list.add(ground);
     let position: Mat4 = Mat4::IDENTITY;
     let p = Primitive::new_alembic("samples/capsule.abc".to_string(), position, 0, true);
-    let teapot_material = crust_render::Disney::new(
+    let teapot_material = crust_core::Disney::new(
         Vec3A::new(0.5, 0.5, 0.5),
         0.0,
         0.2,
@@ -62,7 +62,7 @@ fn main() {
             center: light_center,
             radius: light_radius,
         },
-        MaterialType::Emissive(crust_render::Emissive::new(
+        MaterialType::Emissive(crust_core::Emissive::new(
             Vec3A::new(10.0, 10.0, 10.0),
             light_center,
             light_radius,
@@ -76,7 +76,7 @@ fn main() {
             center: light_center,
             radius: light_radius,
         },
-        MaterialType::Emissive(crust_render::Emissive::new(
+        MaterialType::Emissive(crust_core::Emissive::new(
             Vec3A::new(20.0, 10.0, 7.0),
             light_center,
             light_radius,
